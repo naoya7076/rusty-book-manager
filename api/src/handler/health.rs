62446041530c05e323ec6a5011f54f58982a0526
naoya7076 <naoya7076@gmail.com>
2025-02-14 22:30:35 +0900

    ハンドラの実装

---
+++ api/src/handler/health.rs
@@ -0,0 +1,10 @@
+use axum::{extract::State, http::StatusCode};
+use registry::AppRegistry;
+
+pub async fn health_check_db(State(registry): State<AppRegistry>) -> StatusCode {
+    if registry.health_check_repository().check_db().await {
+        StatusCode::OK
+    } else {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
