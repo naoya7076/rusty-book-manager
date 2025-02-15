5a76f6d4ed47db55a7a71bc65efe6de938421565
naoya7076 <naoya7076@gmail.com>
2025-02-15 22:17:15 +0900

    updated_atを更新するプロシージャを作成

--- adapter/migrations/20250215024220_start.up.sql
+++ adapter/migrations/20250215024220_start.up.sql
@@ -1,1 +1,7 @@
--- Add up migration script here
+CREATE OR REPLACE FUNCTION set_updated_at() RETURNS TRIGGER AS '
+  BEGIN
+    new.updated_at = NOW();
+    RETURN new;
+  END;
+' LANGUAGE 'plpgsql';
+
