3b55fddc7c5f4f138fc2d884270df2ba2d80b4d0
naoya7076 <naoya7076@gmail.com>
2025-02-17 17:33:37 +0900

    蔵書一覧を取得するメソッド

--- api/src/handler/book.rs
+++ api/src/handler/book.rs
@@ -2,7 +2,7 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use registry::AppRegistry;
 use thiserror::Error;
 
-use crate::model::book::CreateBookRequest;
+use crate::model::book::{BookResponse, CreateBookRequest};
 #[derive(Error, Debug)]
 pub enum AppError {
     #[error("{0}")]
@@ -24,5 +24,17 @@ pub async fn register_book(
         .create(req.into())
         .await
         .map(|_| StatusCode::CREATED)
+        .map_err(AppError::from)
+}
+
+pub async fn show_book_list(
+    State(registry): State<AppRegistry>,
+) -> Result<Json<Vec<BookResponse>>, AppError> {
+    registry
+        .book_repository()
+        .find_all()
+        .await
+        .map(|v| v.into_iter().map(BookResponse::from).collect::<Vec<_>>())
+        .map(Json)
         .map_err(AppError::from)
 }
