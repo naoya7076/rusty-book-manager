00740d7f7e9a8d493d442b8d580ba321ca3038b0
naoya7076 <naoya7076@gmail.com>
2025-02-17 16:53:50 +0900

    蔵書登録処理を追加

--- api/src/handler/book.rs
+++ api/src/handler/book.rs
@@ -1,5 +1,8 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use registry::AppRegistry;
 use thiserror::Error;
+
+use crate::model::book::CreateBookRequest;
 #[derive(Error, Debug)]
 pub enum AppError {
     #[error("{0}")]
@@ -10,4 +13,16 @@ impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
         (StatusCode::INTERNAL_SERVER_ERROR, "").into_response()
     }
+}
+
+pub async fn register_book(
+    State(registry): State<AppRegistry>,
+    Json(req): Json<CreateBookRequest>,
+) -> Result<StatusCode, AppError> {
+    registry
+        .book_repository()
+        .create(req.into())
+        .await
+        .map(|_| StatusCode::CREATED)
+        .map_err(AppError::from)
 }
