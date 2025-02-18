b05cf606f5a5d2e74c35caf581941fbf080a687f
naoya7076 <naoya7076@gmail.com>
2025-02-18 14:54:31 +0900

    ログ出力の実装

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -5,11 +5,16 @@ use anyhow::{Error, Result};
 use api::route::{book::build_book_routers, health::build_health_check_routers};
 use axum::Router;
 use registry::AppRegistry;
-use shared::config::AppConfig;
+use shared::{
+    config::AppConfig,
+    env::{which, Environment},
+};
 use tokio::net::TcpListener;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger()?;
     bootstrap().await
 }
 
@@ -26,4 +31,22 @@ async fn bootstrap() -> Result<()> {
     let listener = TcpListener::bind(&addr).await?;
     println!("Listening on {}", addr);
     axum::serve(listener, app).await.map_err(Error::from)
+}
+
+fn init_logger() -> Result<()> {
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(subscriber)
+        .try_init()?;
+    Ok(())
 }
